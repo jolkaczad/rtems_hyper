6038fff3e15a2db385f4dff7a28a6c34f0eae539
Wiktor Langowski <jolkaczad@gmail.com>
2012-07-22 00:25:55 +0800

    got the compilation sorted out a bit, still needs the custom compilation script but got rid of unresolved symbols

--- rtems_wspace/hello_world_c/collect_script.sh
+++ rtems_wspace/hello_world_c/collect_script.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-#/opt/rtems-4.11/libexec/gcc/i386-rtems4.11/4.6.3/ld -o o-optimize/hello.ralf \
-/opt/rtems-4.11/bin/i386-rtems4.11-ld -nostdlib -M=Wl.map -o o-optimize/hello.ralf \
-o-optimize/test.o \
--L$POK_PATH/libpok -L$RTEMS_WSPACE_PATH/b-pok/install/i386-rtems4.11/pok/lib \
--L/opt/rtems-4.11/lib/gcc/i386-rtems4.11/4.6.3 \
--L/opt/rtems-4.11/lib/gcc/i386-rtems4.11/4.6.3/../../../../i386-rtems4.11/lib \
--lgcc --start-group -lrtemsbsp -lrtemscpu -lc -lgcc -lpok \
---end-group -T $RTEMS_WSPACE_PATH/b-pok/install/i386-rtems4.11/pok/lib/linkcmds -lgcc
+i386-rtems4.11-gcc -L$POK_PATH/libpok --pipe -B$RTEMS_WSPACE_PATH/b-pok/install/i386-rtems4.11/pok/lib/ -specs bsp_specs -qrtems   -g -Wall  -g              -o o-optimize/hello.exe  o-optimize/test.o -lpok
